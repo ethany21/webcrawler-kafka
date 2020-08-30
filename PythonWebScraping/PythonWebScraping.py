a84b9edcb5b77fa3c36040564233c777bd2d60c7
ethany21 <imwoodam@hanmail.net>
2020-08-31 01:43:03 +0900

    Scraping Ruliweb

--- PythonWebScraping/PythonWebScraping.py
+++ PythonWebScraping/PythonWebScraping.py
@@ -3,6 +3,7 @@ import re
 import urllib.request
 from dataScrapping import parseContent
 import time
+import json
  
 
 pattern = re.compile('[0-9]+')
@@ -51,26 +52,28 @@ pattern = re.compile('[0-9]+')
 
 #    print(key_num, category, author, title, content)
 
-def cronSched():
 
-    temp = []
+######## HIPHOPLE에 새로운 글이 올라오면 스크랩해서 return 하는 함수 ###########
+
+def ScrapLE():
+    difference = 0
+    output = []
+
     soup_start = BeautifulSoup(urllib.request.urlopen("http://hiphople.com/kboard").read(), "html.parser")
     for div in soup_start.find_all("tr", class_="notice"):
         div.decompose()
-    start = int(soup_start.find_all("td", class_="no")[0].text.strip())
-    print(start)
+    title_temp = soup_start.find_all("td", class_="title")
+
     time.sleep(600)
+
     soup_end = BeautifulSoup(urllib.request.urlopen("http://hiphople.com/kboard").read(), "html.parser")
     for div in soup_end.find_all("tr", class_="notice"):
         div.decompose()
-    end = int(soup_end.find_all("td", class_="no")[0].text.strip())
-    print(end)
+ 
+    title_compare = soup.end.find_all("td", class_="title")
 
-    len = end - start
-
-    if len > 0:
-        for n in range(0, len):
-
+    for n in range(0, len(title_compare)):
+        if title_compare[n] not in title_temp:
             key_num = pattern.findall(soup_end.find_all("td", class_="no")[n].text)
             category = soup_end.find_all("td", class_="categoryTD")[n].find("span").text
             author = soup_end.find_all("td", class_="author")[n].find("span").text
@@ -80,12 +83,104 @@ def cronSched():
             content = str(soup2.find_all("div", class_="article-content")[0].find_all("p"))
             content = re.sub("<.+?>","", content,0).strip()
             content = re.sub("\xa0","", content, 0).strip()
-
-            temp.append(title)
-
-            print(key_num, category, author, title, content)
+            output.append({"number": key_num, "category": category, "author": author, "title": title, "content": content})
+            difference += 1
+    
+    if difference > 0:
+        for str in output:
+            yield str
     else:
-        print("no newly created content")
+        return "there is no newly created content"
 
-while(True):
-    cronSched()
+######## 루리웹에 새로운 글이 올라오면 스크랩해서 return 하는 함수 ###########
+def scrapRuliweb():
+    temp = []
+    output = []
+    result = []
+    soup1 = BeautifulSoup(urllib.request.urlopen("https://bbs.ruliweb.com/best/board/300143").read(), "html.parser")
+    soup_start = soup1.find("div", class_= "board_main theme_default")
+
+    for div in soup_start.find_all("tr", class_="table_body notice inside"):
+        div.decompose()
+    for div in soup_start.find_all("tr", class_="table_body best inside"):
+        div.decompose()
+    for div in soup_start.find_all("tr", class_="table_body list_inner"):
+        div.decompose()
+
+    title_temp = soup_start.find_all("div", class_="relative")
+
+    for title in title_temp:
+        temp.append(title.find("a").text.strip())
+
+    time.sleep(60)
+    
+    soup2 = BeautifulSoup(urllib.request.urlopen("https://bbs.ruliweb.com/best/board/300143").read(), "html.parser")
+    soup_last = soup2.find("div", class_= "board_main theme_default")
+
+    for div in soup_last.find_all("tr", class_="table_body notice inside"):
+        div.decompose()
+    for div in soup_last.find_all("tr", class_="table_body best inside"):
+        div.decompose()
+    for div in soup_last.find_all("tr", class_="table_body list_inner"):
+        div.decompose()
+
+    key_num = soup_last.find_all("td", class_="id")
+    category = soup_last.find_all("td", class_="divsn")
+    author = soup_last.find_all("td", class_="writer text_over")
+    title = soup_last.find_all("div", class_="relative")
+    link = soup_last.find_all("div", class_="relative")
+       
+    for x, y, z, t, u in zip (key_num, category, author, title, link):
+        hyperlink = u.find("a").attrs["href"]
+        soup2 = BeautifulSoup(urllib.request.urlopen(hyperlink).read(), "html.parser")
+        content = str(soup2.find_all("div", class_="view_content")[0].find("div"))
+        content = re.sub("<.+?>","", content,0).strip()
+        content = re.sub("\xa0","", content, 0).strip()
+        output.append({"number": x.text.strip(), "category": y.text.strip(), "author": z.find("a").text.strip().replace('\xad', ''), "title": t.find("a").text.strip(), "content": content.replace('\n', '')})
+    
+    
+
+    for check in output:
+        if check["title"] not in temp:
+            result.append(check)
+
+    if len(result) > 0:
+        for string in result:
+            yield string
+    else:
+        return "there is no newly created content"
+    
+############ 루리웹 첫 페이지 스크랩 테스트용 함수 ########
+def checkRuliweb():
+    soup = BeautifulSoup(urllib.request.urlopen("https://bbs.ruliweb.com/best/board/300143").read(), "html.parser")
+    soup_end = soup.find("div", class_= "board_main theme_default")
+
+    for div in soup_end.find_all("tr", class_="table_body notice inside"):
+        div.decompose()
+    for div in soup_end.find_all("tr", class_="table_body best inside"):
+        div.decompose()
+    for div in soup_end.find_all("tr", class_="table_body list_inner"):
+        div.decompose()
+
+    key_num = soup_end.find_all("td", class_="id")
+    category = soup_end.find_all("td", class_="divsn")
+    author = soup_end.find_all("td", class_="writer text_over")
+    title = soup_end.find_all("div", class_="relative")
+    link = soup_end.find_all("div", class_="relative")
+       
+    for x, y, z, t, u in zip (key_num, category, author, title, link):
+        hyperlink = u.find("a").attrs["href"]
+        soup2 = BeautifulSoup(urllib.request.urlopen(hyperlink).read(), "html.parser")
+        content = str(soup2.find_all("div", class_="view_content")[0].find("div"))
+        content = re.sub("<.+?>","", content,0).strip()
+        content = re.sub("\xa0","", content, 0).strip()
+        yield {"number": x.text.strip(), "category": y.text.strip(), "author": z.find("a").text.strip().replace('\xad', ''), "title": t.find("a").text.strip(), "content": content.replace('\n', '')}
+
+
+
+
+#while(True):
+#    temp = scrapRuliweb()
+#    for i in temp:
+#        print(i)
+#    print("#################################################################################################################################################################")
